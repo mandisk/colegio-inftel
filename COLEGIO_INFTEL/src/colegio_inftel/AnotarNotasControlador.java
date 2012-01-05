6e628d29c9cff5e071590d10abf5c8b51975ea65
agumpg <agumpg@3de28c87-e37a-1234-a174-6150c2a18ef5>
2012-01-05 14:14:11 +0000

    actualizacion clases AnotarNotasxxx

--- COLEGIO_INFTEL/src/colegio_inftel/AnotarNotasControlador.java
+++ COLEGIO_INFTEL/src/colegio_inftel/AnotarNotasControlador.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import javax.print.attribute.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 
 /**
  * Controlador para la vista/modelo Anotar Notas
@@ -25,7 +28,7 @@ public class AnotarNotasControlador {
 
         m_vista.addGuardarListener(new GuardarListener());
         m_vista.addCerrarListener(new CerrarListener());
-        m_vista.addCambioValorNotaListener(new CambioNotaListener());
+
     }
 
 
@@ -53,6 +56,25 @@ public class AnotarNotasControlador {
         public void propertyChange(PropertyChangeEvent evt) {
             System.out.println("el valor ha cambiado");
         }
+    }
+
+
+    class LimitadorCaracteres extends PlainDocument
+    {
+       /**
+        * Método al que llama el editor cada vez que se intenta insertar caracteres.
+        * Sólo debemos verificar arg1, que es la cadena que se quiere insertar en el JTextField
+        */
+       public void insertString(int arg0, String arg1, AttributeSet arg2) throws BadLocationException
+       {
+           for (int i=0;i<arg1.length();i++)
+              // si no es digit, volvemos
+              if (!Character.isDigit(arg1.charAt(i)))
+                 return;
+
+           // Si todos son digit, insertamos el texto en el JTextField
+           super.insertString(arg0, arg1,(javax.swing.text.AttributeSet) arg2);
+       }
     }
 
 
