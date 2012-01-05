fddfb23197d935103e68f380dd18973fa39916b8
agumpg <agumpg@3de28c87-e37a-1234-a174-6150c2a18ef5>
2012-01-05 08:07:09 +0000

    javadoc de la clase ConfigBD.java

--- trunk/COLEGIO_INFTEL/src/colegio_inftel/ConfigBD.java
+++ trunk/COLEGIO_INFTEL/src/colegio_inftel/ConfigBD.java
@@ -1,6 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ConfigDB.java
+ *
  */
 
 package colegio_inftel;
@@ -11,17 +11,32 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
+ * Utilidad para obtener los datos de configuración de la Base de Datos
+ * a partir de un fichero de propiedades
  *
- * @author agumpg
+ * @author Agustín Pereña
+ * @author Manuel Valls
+ * @author Jesús Barriga
+ * @author Luis Jarén
+ * @version v1.0 Diciembre-2011
  */
 public class ConfigBD {
 
     private Properties p;
 
+    /**
+     * Atributos miembros para la carga y lectura posterior de los datos
+     * de configuración de la Base de Datos
+     */
     private String uri;
     private String usuario;
     private String clave;
 
+    /**
+     * Constructor de la clase
+     * @param fileName nombre del fichero properties con los datos de conexión
+     *                 a la Base de Datos
+     */
     public ConfigBD(String fileName) {
 
         p = new Properties();
@@ -43,15 +58,26 @@ public class ConfigBD {
 
     }
 
+    /**
+     * Getter del atributo miembro clave
+     * @return (String) clave de conexión a la BD
+     */
     public String getClave() {
         return clave;
     }
 
+    /**
+     * Getter del atributo miembro URI
+     * @return (String) uri de conexión a la BD
+     */
     public String getUri() {
         return uri;
     }
 
-
+    /**
+     * Getter del atributo miembro usuario
+     * @return (String) usuario de la BD
+     */
     public String getUsuario() {
         return usuario;
     }
