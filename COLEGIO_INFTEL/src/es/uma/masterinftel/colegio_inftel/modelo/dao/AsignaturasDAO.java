19ad12475c8e388a578ce74d25d912c2fd76807b
agumpg <agumpg@3de28c87-e37a-1234-a174-6150c2a18ef5>
2012-01-11 01:13:25 +0000

    AsignaturasDAO con los metodos obtener asignturas y obtener asignaturas por profesor

--- COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dao/AsignaturasDAO.java
+++ COLEGIO_INFTEL/src/es/uma/masterinftel/colegio_inftel/modelo/dao/AsignaturasDAO.java
@@ -9,15 +9,17 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import es.uma.masterinftel.colegio_inftel.modelo.dto.AsignaturasDTO;
 import es.uma.masterinftel.colegio_inftel.modelo.dto.CursosDTO;
+import es.uma.masterinftel.colegio_inftel.utilidades.Conexion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
  * @author Agustin Pereña
  */
-public class AsignaturasDAO {
+public class AsignaturasDAO extends GenericDAO {
 
     public static final String SQL_SELECT_ASIGNATURAS =
            "SELECT * FROM ASIGNATURAS;";
@@ -42,10 +44,12 @@ public class AsignaturasDAO {
 
                     AsignaturasDTO dto = new AsignaturasDTO();
                     
-
-                    dto.setId(rs.getInt("id"));
+                    dto.setCodasignatura(rs.getInt("codasignatura"));
                     dto.setDesc(rs.getString("desc"));
-                    cursos.add(dto);
+                    dto.setProfesor_id_fk(rs.getInt("profesor_id_fk"));
+                    dto.setImparte_cursos_id_fk(rs.getInt("imparte_cursos_id_fk"));
+
+                    asignaturas.add(dto);
 
                 }
 
@@ -56,13 +60,86 @@ public class AsignaturasDAO {
             cerrar(ps);
         }
 
-        if (cursos.size() > 0) {
-            return cursos;
+        if (asignaturas.size() > 0) {
+            return asignaturas;
         } else {
             return null;
         }
 
     }
+
+
+
+    public ArrayList<CursosDTO> obtenerAsignaturasByProfesor(Connection conn, Integer id_profesor) throws SQLException {
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ArrayList asignaturas = new ArrayList();
+
+        try {
+            if (conn!=null){
+                ps = (PreparedStatement) conn.prepareStatement(SQL_SELECT_ASIGNATURAS_PROFESOR);
+                ps.setInt(1, id_profesor);
+                rs = ps.executeQuery();
+
+                //Creamos la lista con todos los objetos asignaturas
+                while (rs.next()){
+
+                    AsignaturasDTO dto = new AsignaturasDTO();
+
+                    dto.setCodasignatura(rs.getInt("codasignatura"));
+                    dto.setDesc(rs.getString("desc"));
+                    dto.setProfesor_id_fk(rs.getInt("profesor_id_fk"));
+                    dto.setImparte_cursos_id_fk(rs.getInt("imparte_cursos_id_fk"));
+
+                    asignaturas.add(dto);
+
+                }
+
+            }
+
+        } finally {
+            cerrar(rs);
+            cerrar(ps);
+        }
+
+        if (asignaturas.size() > 0) {
+            return asignaturas;
+        } else {
+            return null;
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) throws SQLException{
+        ArrayList res;
+
+        System.out.println("Probando AsignaturasDAO....");
+
+
+        Connection cnn = (Connection) Conexion.conectar();
+        AsignaturasDAO dao = new AsignaturasDAO();
+
+        //res = dao.obtenerAsignaturas(cnn);
+        res = dao.obtenerAsignaturasByProfesor(cnn, 1);
+
+        Iterator i = res.listIterator();
+        while(i.hasNext()){
+            AsignaturasDTO a = (AsignaturasDTO) i.next();
+
+            System.out.println(a.getCodasignatura()+","+a.getDesc()+","+a.getProfesor_id_fk()+","+a.getImparte_cursos_id_fk());
+
+        }
+
+
+    }
+
+
+
+
 
 
 }
